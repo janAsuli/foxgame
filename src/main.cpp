1961358c9cf5e64569c29d6140b9926511aa810c
Ashley Tessitore <ashleytessitore@proton.me>
2025-01-16 18:17:45 -0500

    Simulate 1 million games

--- src/main.cpp
+++ src/main.cpp
@@ -4,19 +4,31 @@
 
 #include "board.hpp"
 
-int main() {
-    std::srand(std::time(nullptr));
+// Runs a game, and returns whether the player won
+bool runGame() {
     Board board;
     Bag bag;
-    board.output();
     while (!bag.isEmpty()) {
         board.placeTile(bag);
-        board.output();
         if (board.hasFox()) {
-            std::cout << "You Lose!\n";
-            return 0;
+            return false;
         }
-        std::cout << '\n';
     }
-    std::cout << "You win!\n";
+    return true;
+}
+
+int main() {
+    std::srand(std::time(nullptr));
+    // Run x games, and see how many are wins
+    int x = 1'000'000;
+    int wins = 0;
+    int losses = 0;
+    for (int i = 0; i < x; i++) {
+        if (runGame()) {
+            wins++;
+        } else {
+            losses++;
+        }
+    }
+    std::cout << "Wins: " << wins << ", Losses: " << losses << std::endl;
 }
