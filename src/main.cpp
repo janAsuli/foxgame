006db4cb9c14a496b1522d342bc4da5ff0d6873d
Ashley Tessitore <ashleytessitore@proton.me>
2025-01-16 19:40:10 -0500

    Use multiple threads to go faster

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,10 @@
 #include <ctime>
 #include <cstdlib>
 #include <iostream>
+#include <mutex>
 #include <random>
+#include <thread>
+#include <vector>
 
 #include "board.hpp"
 
@@ -18,18 +21,37 @@ bool runGame(std::mt19937_64& rng) {
     return true;
 }
 
-int main() {
-    auto rng = std::mt19937_64(std::time(nullptr));
-    // Run x games, and see how many are wins
-    int x = 1'000'000;
+static int totalWins = 0;
+static int totalLosses = 0;
+static std::mutex resultsLock;
+
+void runTask(int seed, int count, int threadCount, int threadI) {
+    auto rng = std::mt19937_64(seed + threadI);
     int wins = 0;
     int losses = 0;
-    for (int i = 0; i < x; i++) {
+    for (int i = threadI; i < count; i += threadCount) {
         if (runGame(rng)) {
             wins++;
         } else {
             losses++;
         }
     }
-    std::cout << "Wins: " << wins << ", Losses: " << losses << std::endl;
+    std::lock_guard<std::mutex> lock(resultsLock);
+    totalWins += wins;
+    totalLosses += losses;
+}
+
+int main() {
+    int x = 1'000'000;
+    auto threadCount = std::thread::hardware_concurrency();
+    std::vector<std::thread> threadPool;
+    threadPool.reserve(threadCount);
+    int seed = std::time(nullptr);
+    for (int i = 0; i < threadCount; i++) {
+        threadPool.push_back(std::thread(runTask, seed, x, threadCount, i));
+    }
+    for (auto& thread: threadPool) {
+        thread.join();
+    }
+    std::cout << "Wins: " << totalWins << ", Losses: " << totalLosses << std::endl;
 }
